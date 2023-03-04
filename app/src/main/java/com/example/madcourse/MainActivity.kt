5c542d0053d681cc30f079496324fca13cbd0446
arpit999 <arpitpatel009@gmail.com>
2023-03-04 13:04:05 -0500

    [Assignment-1] Implemented UI

--- app/src/main/java/com/example/madcourse/MainActivity.kt
+++ app/src/main/java/com/example/madcourse/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.madcourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.madcourse.ui.theme.MADCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +21,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MADCourseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+    val clickCounter by remember { mutableStateOf("CLICK COUNTER") }
+    // A surface container using the 'background' color from the theme
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        AppContent(clickCounter)
+    }
+}
+
+
+@Composable
+fun AppContent(clickCounter: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = clickCounter)
+        Spacer(modifier = Modifier.padding(vertical = 8.dp))
+        Button(
+            border = BorderStroke(3.dp, Color.Blue.copy(alpha = 0.5f)),
+            shape = RectangleShape,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White,
+                contentColor = Color.Black
+            ),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = "BUTTON", fontWeight = FontWeight.Medium)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MADCourseTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
